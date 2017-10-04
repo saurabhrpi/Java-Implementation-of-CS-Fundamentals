4cb4866f581527e7a68079d58cc852bb45ab681f
Saurabh Sharma <saurabhsvits@gmail.com>
2017-10-04 14:20:58 -0400

    Create URLify.java
    replace space with '%20'

---
+++ URLify.java
@@ -0,0 +1,29 @@
+public class URLify {
+    public static void main(String args[]) {
+        char [] ch = {'M','r',' ','J','o','h','n',' ','S','m','i','t','h',' ',' ',' ',' ',' ',' ',' '};
+        int n = 13;
+        URLify u = new URLify();
+        u.replaceChar(ch,n);
+        System.out.println(ch);
+    }
+    public void replaceChar(char[] c, int n){
+        char swap;
+        int start = n - 1;
+        int end = start;
+        for(;start>0;start--)
+        {
+            if(c[start] == ' '){
+                int mark = end;
+                while(mark != start){
+                    swap = c[mark];
+                    c[mark+2] = swap;
+                    mark--;
+                }
+                end += 2;
+                c[start] = '%';
+                c[start+1] = '2';
+                c[start+2] = '0';
+            }
+        }
+    }
+}
