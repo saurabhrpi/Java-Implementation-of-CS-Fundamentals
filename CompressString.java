8a98d8bfcf6a8a15dc486df05124fe286e22442e
Saurabh Sharma <saurabhsvits@gmail.com>
2017-10-06 09:33:17 -0400

    CTCI 1.6
    String Compression: Implement a method to perform basic string compression using the counts
    of repeated characters. For example, the string aabcccccaaa would become a2blc5a3. If the
    "compressed" string would not become smaller than the original string, your method should return
    the original string. You can assume the string has only uppercase and lowercase letters (a - z).

---
+++ CompressString.java
@@ -0,0 +1,45 @@
+public class CompressString {
+    public void compressIt(StringBuilder sb){
+	    int[] ind = new int[sb.length()];
+	    int j = 0;
+	    ind[j] = 1;
+	    char[] ch = new char[sb.length()];
+	    ch[j] = sb.charAt(j);
+	    for(int i=0; i < sb.length() - 1; i++){
+	        if(sb.charAt(i) == sb.charAt(i+1)){
+	            ind[j]++;        
+	        }
+	        else{
+	            j++;
+	            ind[j] = 1;
+	            ch[j] = sb.charAt(i+1);
+	        }
+	    }
+	    
+	    // Reusing the stringbuilder since recreating it would be even slower.
+	    // https://codereview.stackexchange.com/a/7637
+	    
+	    sb = sb.delete(0,sb.length()); 
+	    
+	    for(int i=0; i < ind.length; i++){
+	        if(ch[i] != '\u0000' && ind[i] > 0){
+	            sb.append(ch[i]);
+	            sb.append(ind[i]);
+	        }
+	    }
+    }    
+    
+    public static void main(String args[]) {
+        String inp = "aaabbccccaae";
+        StringBuilder sb = new StringBuilder(inp);
+        CompressString c = new CompressString();
+        c.compressIt(sb);
+    	if(sb.length() >= inp.length()){
+    	    System.out.println("Compression was not effective. Here is the original string " + inp);
+    	}
+    	else{
+    	    System.out.println("Compression successful. Here is the new string " + sb);
+    	}
+    }
+}
+
