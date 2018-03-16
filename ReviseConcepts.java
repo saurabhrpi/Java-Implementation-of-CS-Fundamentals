d3144c29a3313a4845f98e49d0d04ad8143a4c93
Saurabh Sharma <saurabhsvits@gmail.com>
2018-03-15 23:14:15 -0400

    Update ReviseConcepts.java
    Finish the permutation checker code after implementing Heapsort

--- ReviseConcepts.java
+++ ReviseConcepts.java
@@ -7,12 +7,32 @@ public class PermutationChecker {
             return false;
         }
         
-	 //call heapsort
-	    
+    	 HeapSort hs = new HeapSort();
+    	 s1 = hs.sort(s1);
+    	 s2 = hs.sort(s2);  
+    	 
+    	for(int i=0; i < s1.length(); i++)
+    	{
+    	    if(s1.charAt(i) != s2.charAt(i))
+    	    {
+    	        return false;
+    	    }
+    	}
+    	return true;
     }
     public static void main(string args[]) 
     {
-        
+        string s1 = 'abrakadabra';
+        string s2 = 'dabrakaabra';
+        PermutationChecker pc = new PermutationChecker();
+        if(!pc.check(s1,s2))
+        {
+            System.out.println("The strings are not a permutation of each other.");
+        }
+        else
+        {
+            System.out.println("The strings are indeed a permutation of each other.");
+        }
     }
 }
 
