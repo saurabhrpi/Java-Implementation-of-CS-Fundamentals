033cfc71efd0d46875e0c108fabb028db6d0f0bd
Saurabh Sharma <saurabhsvits@gmail.com>
2018-03-17 21:20:55 -0400

    Update ReviseConcepts.java
    Unit test and fix the compile time as well as runtime errors.

--- ReviseConcepts.java
+++ ReviseConcepts.java
@@ -1,5 +1,5 @@
 public class PermutationChecker {
-    public boolean check(string s1, string s2)
+    public boolean check(String s1, String s2)
     {
         if(s1.length() != s2.length())
         {
@@ -11,6 +11,9 @@ public class PermutationChecker {
     	 s1 = hs.sort(s1);
     	 s2 = hs.sort(s2);  
     	 
+    	 System.out.println("s1 :" + s1);
+    	 System.out.println("s2 :" + s2);
+    	 
     	for(int i=0; i < s1.length(); i++)
     	{
     	    if(s1.charAt(i) != s2.charAt(i))
@@ -20,10 +23,10 @@ public class PermutationChecker {
     	}
     	return true;
     }
-    public static void main(string args[]) 
+    public static void main(String args[]) 
     {
-        string s1 = 'abrakadabra';
-        string s2 = 'dabrakaabra';
+        String s1 = "abrakadabra";
+        String s2 = "dabrakaabra";
         PermutationChecker pc = new PermutationChecker();
         if(!pc.check(s1,s2))
         {
@@ -37,40 +40,42 @@ public class PermutationChecker {
 }
 
 class HeapSort{
- public string sort(string str)
+ public String sort(String str)
     {
-        if(str == null or str.length() == 0)
+        if(str == null || str.length() == 0)
         {
             return "Invalid String";
         }
         char[] ch = str.toCharArray();	
+        int n = ch.length;
         for(int i = n/2 - 1; i >= 0; i--)
         {
             heapify(ch, i);    
         }
-        
+        String ret = new String(ch);
+        return ret;
     }
  public void heapify(char[] ch, int i) 	
     {
-	int largest = i;
-	int left = 2*i + 1;
-	int right = 2*i + 2;
-	if(i < ch.length && ch[i] < ch[left])
-	{
-		largest = left;
-	}
-	 
-	if(i < ch.length && ch[largest] < ch[right])
-	{
-		largest = right;
-	}
-	 
-	if(largest != i)
-	{
-		int swap = ch[largest];
-		ch[largest] = ch[i];
-		ch[i] = swap;
-		heapify(ch, largest);
-	}
+    	int largest = i;
+    	int left = 2*i + 1;
+    	int right = 2*i + 2;
+    	if(left < ch.length && ch[i] < ch[left])
+    	{
+    		largest = left;
+    	}
+    	 
+    	if(right < ch.length && ch[largest] < ch[right])
+    	{
+    		largest = right;
+    	}
+    	 
+    	if(largest != i)
+    	{
+    		char swap = ch[largest];
+    		ch[largest] = ch[i];
+    		ch[i] = swap;
+    		heapify(ch, largest);
+    	}
     }	
 }
