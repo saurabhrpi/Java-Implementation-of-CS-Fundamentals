2e35ca333da3921703dae614a919460ec6520f71
Saurabh Sharma <saurabhsvits@gmail.com>
2018-03-24 17:22:10 -0400

    Implementation : Update with a better version
    Update with a cleaner and simpler version.

--- EditDiff.java
+++ EditDiff.java
@@ -1,41 +1,61 @@
-public class EditDiff{
-    public boolean editDiff(String s1, String s2){
-        if(Math.abs(s1.length() - s2.length()) > 1){
+public class EditDiffChecker{
+    
+    public boolean diffChecker(String str1, String str2)
+    {
+        if(Math.abs(str1.length() - str2.length()) > 1)
+        {
             return false;
         }
-        int[] char_set = new int[128];// assuming ASCII
         
-        for(int j=0; j < s1.length(); j++){
-            char_set[s1.charAt(j)]++;
+        int[] ch = new int[128];
+    
+        for(int i=0; i < str1.length(); i++)
+        {
+            ch[str1.charAt(i)]++;
+            System.out.println(str1.charAt(i));
+            System.out.println(ch[str1.charAt(i)]);
         }
-
-        boolean thresh = false;
-        for(int j = 0; j < s2.length(); j++){
-            char_set[s2.charAt(j)]--;
-            if(char_set[s2.charAt(j)] < 0){
-                if(thresh){
-                 return false;
+        
+        for(int i=0; i < str2.length(); i++)
+        {
+            ch[str2.charAt(i)]--;
+            System.out.println(str2.charAt(i));
+            System.out.println(ch[str2.charAt(i)]);
+        }
+        
+        System.out.println("Checking count diff");
+        
+        int missingChars = 0;
+        for(int i=0; i < ch.length ; i++)
+        {
+            if(ch[i] != 0)
+            {
+                System.out.println((char)i);
+                System.out.println(ch[i]);
+                if(missingChars > 2)
+                {
+                    return false;
                 }
-                    thresh = true;
+                missingChars++;   
             }
         }
-        if(thresh && s1.length() > s2.length()){
-            return false;   
-        }
-        return true;
+        
+        return true;   
     }
     
-    public static void main(String args[]) {
-        // assuming the inputs will be of same case
-        String s1 = "pale";
-        String s2 = "bae";
-        EditDiff e = new EditDiff();
-        if(e.editDiff(s1,s2)){
-            System.out.println("Given strings differ by one difference at max");
+    public static void main(String[] args)
+    {
+        String input1 = "pale";
+        String input2 = "bae";
+        EditDiffChecker e = new EditDiffChecker();
+        
+        if(e.diffChecker(input1,input2))
+        {
+            System.out.println("The strings differ by exactly one edit");
         }
-        else{
-            System.out.println("Given strings differ by more than the allowed difference");
+        else
+        {
+            System.out.println("The strings do NOT differ by exactly one edit");
         }
     }
 }
- 
