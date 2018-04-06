3d13b6d5a47484f9546a24e990a7e72416f82a7b
Saurabh Sharma <saurabhsvits@gmail.com>
2018-04-05 23:33:35 -0400

    Implementation : Skeleton Creation
    Create the public class, main method, basic test case and the skeleton for the method to implement the functionality.

--- ReturnKthToLast.java
+++ ReturnKthToLast.java
@@ -1,1 +1,41 @@
 # Implement an algorithm to find the kth to last element of a singly linked list
+
+class Node {
+    Node next = null;
+    int data;
+
+    public Node(int d)
+    {
+        data = d;
+    }
+    
+    public void appendToTail(int d)
+    {
+        Node end = new Node(d);
+        Node n = this;
+        while(n.next != null)
+        {
+            n = n.next;
+        }
+        n.next = end;
+    }
+}
+
+public class ReturnKthToLast{
+  public Node returnElement(int k)
+  {
+  
+  }
+  
+  public static void main(String[] args)
+    {
+        //18,10,9,12,10,18
+        Node n = new Node(18);
+        n.appendToTail(10);
+        n.appendToTail(9);
+        n.appendToTail(12);
+        n.appendToTail(10);
+        n.appendToTail(18);       
+               
+    }
+}
