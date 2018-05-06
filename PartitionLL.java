3f7b6260f4da11b2c786424e615502d10879ebff
Saurabh Sharma <saurabhsvits@gmail.com>
2018-05-05 23:03:16 -0400

    Docs : Comments about problem
    Add details of the problem solved as comments at the top

--- PartitionLL.java
+++ PartitionLL.java
@@ -1,3 +1,10 @@
+    /*
+    To partition a linked list around a value x, such that all nodes less than x come
+    before all nodes greater than or equal to x. If x is contained within the list the values of x only need
+    to be after the elements less than x. The partition element x can appear anywhere in the
+    "right partition"; it does not need to appear between the left and right partitions
+    */
+
 class Node {
     Node next = null;
     int data;
