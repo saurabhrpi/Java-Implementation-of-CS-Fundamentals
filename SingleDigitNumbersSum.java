48ad2b7789c1a5d15d3c4b577c40ed5af695631d
Saurabh Sharma <saurabhsvits@gmail.com>
2018-05-22 18:47:51 -0400

    Implementation : Add node class
    Create Node class

--- SingleDigitNumbersSum.java
+++ SingleDigitNumbersSum.java
@@ -3,3 +3,24 @@ Consider a scenario where two numbers can be represented by a linked list, where
 digit. The digits are stored in reverse order, such that the 1 's digit is at the head of the list. 
 Write a function that adds the two numbers and returns the sum as a linked list.
 */
+import java.util.*;
+
+public class Node {
+    Node next = null;
+    int data;
+
+    public Node(int d)
+    {
+        data = d;
+    }
+    
+    public void appendToTail(int d)
+    {
+        Node end = new Node(d);
+        Node n = this;
+        while(n.next != null)
+        {
+            n = n.next;
+        }
+        n.next = end;
+    }
