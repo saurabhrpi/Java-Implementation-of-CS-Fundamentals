6d7479add5d5d41b82175815ead387ac2b6b8448
Saurabh Sharma <saurabhsvits@gmail.com>
2018-09-25 10:37:30 -0400

    Update URLify.java
    Add more efficient algorithm.

--- URLify.java
+++ URLify.java
@@ -1,33 +1,70 @@
-// Char array with sufficient space along with the true length of the string is already provided
+import java.io.*;
+import java.util.*;
+import java.util.LinkedList; 
 
-public class URLify {
-    public static void main(String args[]) {
-        char [] ch = {'M','r',' ','J','o','h','n',' ','S','m','i','t','h',' ',' ',' ',' ',' ',' ',' '};
-        int n = 13;
-        URLify u = new URLify();
-        u.replaceChar(ch,n);
-        System.out.println(ch);
-    }
-    public void replaceChar(char[] c, int n){
-        char swap;
-        int start = n - 1;
-        int end = start;
-        for(;start>0;start--)
+// assuming ASCII, spaces ignored
+public class URL{
+    
+    public int countSpaces(char[] ch, int len)
+    {
+        int count = 0;
+        for(int i=0; i<len; i++)
         {
-            if(c[start] == ' '){
-                int mark = end;
-                while(mark != start){
-                    swap = c[mark];
-                    c[mark+2] = swap;
-                    mark--;
+            if(ch[i] == ' ')
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+    
+    public void makeRoom(char[] ch, int len)
+    {       
+        int count = countSpaces(ch, len);
+        int mark = len + 2*count;
+        for(int i = ch.length - 1; i >= 0 ; i--)
+        {           
+            if(ch[i] != ' ' && count != 0)
+            {                
+                ch[mark-1] = ch[i];
+                ch[i] = ' ';                
+                mark--;
+                if(i != 0 && ch[i-1] == ' ')
+                {
+                    mark -= 3;
+                    count--;
                 }
-                end += 2;
-                c[start] = '%';
-                c[start+1] = '2';
-                c[start+2] = '0';
             }
         }
     }
+    
+    public void URLify(char[] ch, int len)
+    {      
+        makeRoom(ch, len);
+        
+        if(len != 0)
+        {
+            for(int i=0; i < ch.length; i++)
+            {
+                if(ch[i] == ' ')
+                {
+                    ch[i] = '%';
+                    ch[i+1] = '2';
+                    ch[i+2] = '0';
+                    i = i+2;
+                }
+            }
+        }
+        
+        
+    }
+    
+    public static void main(String [ ] a)
+    {
+        URL x = new URL();
+        String s = "Mr John Smith    ";
+        char[] ch = s.toCharArray();
+        x.URLify(ch, 13);
+        System.out.println(ch);
+    }
 }
- 
- 
