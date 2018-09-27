d0c0a78e89a79b91ca7710a8a8b42066168fc4ea
Saurabh Sharma <saurabhsvits@gmail.com>
2018-09-27 16:19:22 -0400

    Implementation : Add in-place feature

--- ZeroMatrix.java
+++ ZeroMatrix.java
@@ -1,40 +1,151 @@
-public class ZeroMatrix {
-    public boolean setMatrix(int[][] matrix)
+public class Zeroes{
+    
+    public void makeZeroInPlace(int[][] m)
     {
-        int m = matrix.length;
-        int n = matrix[0].length;
-        boolean[] nullableRow = new boolean[m];
-        boolean[] nullableCol = new boolean[n];
-        for(int i = 0; i < m; i++)
+        if(m != null && m.length > 0)
         {
-            for(int j = 0; j < n; i++)
+            boolean rowHasZero = false;
+            boolean colHasZero = false;
+            
+            for(int i=0; i < m[0].length ; i++)
             {
-                if(matrix[i][j] == 0)
+                if(m[0][i] == 0)
                 {
-                    nullableRow[i] = true;   
-                    nullableCol[j] = true;   
+                    rowHasZero = true;
+                    break;
+                }
+            }
+            
+            for(int i=0; i < m.length ; i++)
+            {
+                if(m[i][0] == 0)
+                {
+                    colHasZero = true;
+                    break;
+                }
+            }
+            
+            for(int i=1; i < m.length; i++)
+            {
+                for(int j=1; j < m[0].length; j++)
+                {
+                    if(m[i][j] == 0)
+                    {
+                        m[i][0] = 0;
+                        m[0][j] = 0;
+                    }
+                }
+            }
+            
+            for(int i = 1; i < m.length ; i++)
+            {
+                if(m[i][0] == 0)
+                {
+                    for(int k = 0; k < m.length; k++)
+                    {
+                        m[i][k] = 0;
+                    }
+                }
+            }
+            
+            for(int i = 1; i < m[0].length ; i++)
+            {
+                if(m[0][i] == 0)
+                {
+                    for(int k = 0; k < m.length; k++)
+                    {
+                        m[k][i] = 0;
+                    }
+                }
+            }
+            
+            if(rowHasZero)
+            {
+                for(int i = 0; i < m.length ; i++)
+                {
+                    m[0][i] = 0;
+                }
+            }
+            
+            if(colHasZero)
+            {
+                for(int i = 0; i < m.length ; i++)
+                {
+                    m[i][0] = 0;
                 }
             }
         }
-        for(i=0 ; i < m ; i++)
+    }
+    
+    public void makeZero(int[][] m)
+    {
+        if(m != null && m.length > 0)
         {
-            if(nullableRow[i])
+            boolean[] rows = new boolean[m.length];
+            boolean[] cols = new boolean[m[0].length];
+            
+            for(int i=0; i < m.length ; i++)
             {
-                for(j=0 ; j < n ; j++)
+                for(int j=0; j < m[0].length; j++)
                 {
-                    matrix[i][j] = 0;
+                    if(m[i][j] == 0)
+                    {
+                        rows[i] = true;
+                        cols[j] = true;
+                    }
+                }
+            }
+            
+            for(int i=0; i < rows.length ; i++)
+            {
+                if(rows[i])
+                {
+                    for(int j=0; j < cols.length ; j++)
+                    {
+                        m[i][j] = 0;   
+                    }
+                }
+            }
+            
+            for(int i=0; i < cols.length ; i++)
+            {
+                if(cols[i])
+                {
+                    for(int j=0; j < rows.length ; j++)
+                    {
+                        m[j][i] = 0;   
+                    }
                 }
             }
         }
-        for(j=0 ; j < n ; j++)
+    }
+    
+    public static void main(String args[]) {
+        int[][] m = {
+            {110, 10, 567},
+            {10,0, 30},
+            {90,1000, 176}
+        };
+        
+        for(int i=0; i < m.length ; i++)
         {
-            if(nullableRow[j])
+            for(int j=0; j < m[0].length; j++)
             {
-                for(i=0 ; i < m ; i++)
-                {
-                    matrix[i][j] = 0;
-                }
+                System.out.println(m[i][j]);
             }
-        }
+        } 
+        
+              System.out.println("************************");
+        
+        Zeroes z = new Zeroes();
+        
+        z.makeZeroInPlace(m);
+        for(int i=0; i < m.length ; i++)
+        {
+            for(int j=0; j < m[0].length; j++)
+            {
+                System.out.println(m[i][j]);
+            }
+        }   
     }
 }
