f82920e31555c4e783a3bf4affa0f01f9b456238
Saurabh Sharma <saurabhsvits@gmail.com>
2018-12-01 20:58:41 -0500

    Implementation : Add track and insert methods

--- RankFromStream.java
+++ RankFromStream.java
@@ -1,26 +1,95 @@
-// Implement the code to rank a number in a given stream of numbers
 import java.lang.*;
 import java.util.*;
 import java.util.stream.Stream;
 
+//3.19
+
+class RankNode{
+    public int left_size = 0;
+    public RankNode left, right;
+    public int data = 0;
+    public RankNode(int d)
+    {
+        data = d;
+    }
+    
+    public void insert(int d)
+    {
+        if(d <= data)
+        {
+            if(left != null)
+            {
+                left.insert(d);
+            }
+            else
+            {
+                left = new RankNode(d);
+            }
+        }
+        else
+        {
+            if(right != null)
+            {
+                right.insert(d);
+            }
+            else
+            {
+                right = new RankNode(d);
+            }
+        }
+    }
+    
+    public int getRank(int d)
+    {
+        if(d == data)
+        {
+            return left_size;
+        }
+        else if( d < data )
+        {
+            if(left == null)
+            {
+                return -1;
+            }
+            else
+            {
+                return left.getRank(d);
+            }
+        }
+        else
+        {
+            int right_rank = right == null? -1 : right.getRank(d);
+            if(right_rank == -1)
+            {
+                return -1;
+            }
+            else
+            {
+                left_size + 1 + right_rank;
+            }
+        }
+    }
+}
+
 public class RankFromStream{
     
-    int count = 0;
-    ArrayList<Integer> stream;
+    RankNode root = null;
     
-    RankFromStream()
+    public void track(int num)
     {
-        stream = new ArrayList<Integer>();
+        if(root == null)
+        {
+            root = new RankNode(num);
+        }
+        else
+        {
+            root.insert(num);
+        }
     }
     
-    public void track(int x)
+    public int getRankOfNumber(int number)
     {
-        stream.add(x);   
-    }
-    
-    public Integer getRankOfNumber(int x)
-    {
-         
+        return root.getRank(number);
     }
     
     public static void main(String[] args)
@@ -41,6 +110,7 @@ public class RankFromStream{
         for(int i=0; i < arr.length; i++)
         {
             rfs.track(arr[i]);
+            System.out.println(rfs.getRankOfNumber(1));
         }
     }
 }
