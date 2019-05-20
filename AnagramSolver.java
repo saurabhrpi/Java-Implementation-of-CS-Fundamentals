843ff1c37771e85269c54fa5eb85a53e9c9587a3
Saurabh Sharma <saurabhsvits@gmail.com>
2019-05-20 00:08:01 -0400

    Add important documentation about algorithm

--- AnagramSolver.java
+++ AnagramSolver.java
@@ -1,90 +1,78 @@
 import java.util.*;
 import java.lang.*;
 
-class HashMapList<T, E> {
-	private HashMap<T, ArrayList<E>> map = new HashMap<T, ArrayList<E>>();
-	
-	/* Insert item into list at key. */
-	public void put(T key, E item) {
-		if (!map.containsKey(key)) {
-			map.put(key, new ArrayList<E>());
-		}
-		map.get(key).add(item);
-	}
-	
-	/* Insert list of items at key. */
-	public void put(T key, ArrayList<E> items) {
-		map.put(key, items);
-	}
-	
-	/* Get list of items at key. */
-	public ArrayList<E> get(T key) {
-		return map.get(key);
-	}
-	
-	/* Check if hashmaplist contains key. */
-	public boolean containsKey(T key) {
-		return map.containsKey(key);
-	}
-	
-	/* Check if list at key contains value. */
-	public boolean containsKeyValue(T key, E value) {
-		ArrayList<E> list = get(key);
-		if (list == null) return false;
-		return list.contains(value);
-	}
-	
-	/* Get the list of keys. */ 
-	public Set<T> keySet() { 
-		return map.keySet(); 
-	}
-	
-	@Override
-	public String toString() {
-		return map.toString();
-	}
+class HashMapList<T,E>
+{
+    HashMap<T, ArrayList<E>> map = new HashMap<T, ArrayList<E>>();
+    
+    public void put(T key, E value)
+    {
+        if(!map.containsKey(key))
+        {
+            map.put(key, new ArrayList<E>());
+        }
+        map.get(key).add(value);
+    }
+    
+    public Set<T> getKeySet()
+    {
+        return map.keySet();
+    }
+    
+    public ArrayList<E> get(T key)
+    {
+        return map.get(key);
+    }
 }
 
-public class AnagramSolverHM{
-
-    public String sort(String str)
+class Anagrams implements Comparator<String>{
+    
+    public String sortArrays(String s)
     {
-        char[] ch = str.toCharArray();
+        if(s == null)
+        {
+            return null;
+        }
+        
+        char[] ch = s.toCharArray();
         Arrays.sort(ch);
         return new String(ch);
     }
     
-    public String[] solve(String[] arr)
+    public int compare(String s1, String s2)
     {
-        HashMapList<String, String> map = new HashMapList<String, String>();
+        return (sortArrays(s1)).compareTo(s2);
+    }
+}
+
+public class GroupAnagrams{
+    
+    public String[] group(String[] inp)
+    {
+        HashMapList<String, String> ht = new HashMapList<String, String>();
         
-        for(int i = 0; i < arr.length; i++)
+        for(String s : inp)
         {
-            String key = sort(arr[i]);
-            map.put(key, arr[i]);
+            String key = s; // Thankfully didn't declare it as StringBuilder otherwise would have ended up modifying s itself.
+            char[] ch = key.toCharArray();
+            Arrays.sort(ch);
+            ht.put(new String(ch), s);
         }
         
-        String[] res = new String[arr.length];
-        
-        Iterator<String> it = map.keySet().iterator();
-        
-        int i = 0;
-        
-        while(it.hasNext())
+        String[] res = new String[inp.length];
+        int index = 0;
+        for(String key : ht.getKeySet())
         {
-            String key = it.next();
-            ArrayList<String> as = map.get(key);
-            for(int j=0; j < as.size(); j++)
+            for(String i : ht.get(key))
             {
-                res[i] = as.get(j);
-                i++;
+                res[index] = i;
+                index++;
             }
         }
         
         return res;
     }
-   
-    
+
     public static void main(String[] args)
     {
         String[] arr = {
