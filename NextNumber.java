2edeed67df1f0613284f302aaf4edf99c8e46e63
Saurabh Sharma <saurabhsvits@gmail.com>
2019-10-20 15:52:10 -0400

    Update NextNumber.java

--- NextNumber.java
+++ NextNumber.java
@@ -6,7 +6,7 @@ Brute force approach : Increment the number or decrement the number by 1 till we
 */
 
 /*
-Optimal approach
+Optimal bit manipulation approach
 */
 
 public class NextNumber{
@@ -14,8 +14,10 @@ public class NextNumber{
     public int getNext(int num)
     {
         int c = num;
-        int c0 = 0; //consecutive rightmost zeros
-        int c1 = 0; //consecutive rightmost ones
+        int c0 = 0; //consecutive rightmost trailing zeros
+        int c1 = 0; //consecutive rightmost non-trailing ones
+        
+        // order of while loops below is important. We don't want to miss counting 0s if they appear rightmost
         
         while(((c & 1) == 0) && (c != 0)) 
         {
@@ -23,7 +25,7 @@ public class NextNumber{
             c >>= 1;
         }
         
-        while(((c & 1) == 1) && (c != 0)) 
+        while(((c & 1) == 1) ) //&& (c != 0)) // No need to check for zero as it can never be = 1 & = 0.
         {
             c1++;
             c >>= 1;
@@ -40,6 +42,37 @@ public class NextNumber{
         n &= ~((1 << p) - 1); // cleared all bits from position p-1 through 0
         n |= ((1 << (c1 - 1)) - 1); // just add c1 - 1 ones
         return n;
+    }
+    
+    public int getPrev(int num)
+    {
+        int temp = num;
+        int c0 = 0; // non-trailing zeros
+        int c1 = 0; // trailing ones
+        
+        while((temp & 1) == 1)
+        {
+            c1++;
+            temp >>= 1;
+        }
+        
+        if(temp == 0)
+            return -1; // no zeros exist
+        
+        while(((temp & 1) == 0) && temp != 0)    
+        {
+            c0++;
+            temp >>= 1;
+        }
+        
+        int p = c0 + c1;
+        int x = ~0;
+        num &= (x << (p + 1)); // cleared all the bits from position p to 0
+        
+        int mask = (1 << (c1 + 1)) - 1; // for c1+1 ones immediately to the right of p
+        num |= mask << (c0 - 1); // for c0-1 zeros immediately following above ones
+        
+        return num;
     }
     
     public static void main(String args[]) 
