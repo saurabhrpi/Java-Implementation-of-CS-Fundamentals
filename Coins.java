fbe49ad57fc4e983749e86e6ba5cedbb63cc068b
Saurabh Sharma <saurabhsvits@gmail.com>
2019-11-07 16:35:39 -0500

    Create Coins.java

---
+++ Coins.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+
+public class Coins{
+    
+    public int countWays(int n)
+    {
+        if(n == 0) return 0;
+        if(n == 1) return 1;
+        
+    }
+    
+    public static void main(String[] args)
+    {
+        
+    }
+}
