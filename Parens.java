eb47a2c7c27868ea9d71f4ea091a92e7d79f3738
Saurabh Sharma <saurabhsvits@gmail.com>
2019-11-10 20:16:31 -0500

    Update Parens.java
    Add optimal

--- Parens.java
+++ Parens.java
@@ -4,7 +4,9 @@ import java.io.*;
 
 public class Parens{
     
-    Set<String> generateParens(int remaining)
+    // ** brute force **
+    
+    Set<String> generateParensN(int remaining)
     {
         Set<String> set = new HashSet<String>();
         if(remaining == 0)
@@ -13,7 +15,7 @@ public class Parens{
         }
         else
         {
-            Set<String> prev = generateParens(remaining - 1);
+            Set<String> prev = generateParensN(remaining - 1);
             for(String str : prev)
             {
                 for(int i = 0; i < str.length(); i++)
@@ -35,6 +37,37 @@ public class Parens{
         String left = str.substring(0, li);
         String right = str.substring(li);
         return left + "()" + right;
+    }
+    
+    // *** Optimal ***
+    
+    // Post-order traversal
+    // In the final strings, a right (and the rest) will get added for the first time after all the lefts have been added. 
+    // Followed by moving up one level in the leftMost branch, which will then cause leftmost right to move 1 index left and so on. 
+    public void addParens(ArrayList<String> list, int leftRem, int rightRem, char[] str, int index)
+    {
+        if(leftRem < 0 || rightRem < leftRem) return;// invalid state
+        
+        if(leftRem == 0 && rightRem == 0) 
+        {
+            list.add(String.copyValueOf(str));
+        }
+        else
+        {
+            str[index] = '(';
+            addParens(list, leftRem - 1, rightRem, str, index + 1);
+            
+            str[index] = ')';
+            addParens(list, leftRem, rightRem - 1, str, index + 1);
+        }
+    }
+    
+    ArrayList<String> generateParens(int count)
+    {
+        char[] str = new char[count*2];
+        ArrayList<String> list = new ArrayList<String>();
+        addParens(list, count, count, str, 0);
+        return list;
     }
     
     public static void main(String[] args)
