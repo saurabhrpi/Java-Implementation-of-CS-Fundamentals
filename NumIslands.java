403871c5ab2b3d3f7d2323b7277a6af0afab329a
Saurabh Sharma <saurabhsvits@gmail.com>
2021-11-22 00:51:30 -0500

    Update NumIslands.java
    https://leetcode.com/problems/number-of-islands

--- NumIslands.java
+++ NumIslands.java
@@ -1,50 +1,29 @@
 import java.util.*;
-import java.lang.*;
 
-public class NumIslands{
-    
-    public int numIslands(char[][] inp)
-    {
-        int count = 0;
-        for(int i=0; i < inp.length; i++)
-        {
-            for(int j=0; j < inp.length; j++)    
-            {
-                if(inp[i][j] == '1')
-                {
-                    shrink(inp, i, j);
-                    ++count;
-                }
-            }
-        }
-        return count;
+class Solution {
+    public int numIslands(char[][] grid) {                
+       int islands = 0;
+       for(int i = 0; i < grid.length; i++)
+       {
+           for(int j = 0; j < grid[0].length; j++)
+           {
+               if(grid[i][j] == '1')
+               {
+                    markAndDrownIslands(grid, i, j);
+                    islands++;   
+               }               
+           }
+       }
+       return islands;
     }
     
-    public void shrink(char[][] inp, int i, int j)
+    public void markAndDrownIslands(char[][] grid, int i, int j)
     {
-        if(i < 0 || j < 0 || i >= inp.length || j >= inp[i].length || inp[i][j] == '0')
-        {
-            return;
-        }
-        
-        inp[i][j] = '0';
-        
-        shrink(inp, i+1, j);
-        shrink(inp, i-1, j);
-        shrink(inp, i, j+1);
-        shrink(inp, i, j-1);
-    }
-    
-    public static void main(String[] args)
-    {
-        char[][] m = 
-        {
-            {'1','1','0','0','0'},
-            {'1','1','0','0','0'},
-            {'0','0','1','0','0'},
-            {'0','0','0','1','1'}
-        };
-        
-        System.out.println((new NumIslands()).numIslands(m));
+        if(i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] != '1') return;
+        grid[i][j] = '0';
+        markAndDrownIslands(grid, i, j + 1);
+        markAndDrownIslands(grid, i + 1, j);
+        markAndDrownIslands(grid, i - 1, j);
+        markAndDrownIslands(grid, i, j - 1);
     }
 }
