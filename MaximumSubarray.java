1122e67827056b596cd516c95e7bd9c371b30a76
Saurabh Sharma <saurabhsvits@gmail.com>
2021-12-06 20:34:33 -0500

    Added Kadane's algorithm
    https://leetcode.com/problems/maximum-subarray/

--- MaximumSubarray.java
+++ MaximumSubarray.java
@@ -1,30 +1,41 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        if(nums == null || nums.length == 0)
-        {
-            return Integer.MIN_VALUE;
-        }        
+        // My practice of given solution 
+        if(nums.length == 1)
+            return nums[0];
+        int currentSubArray = nums[0];
+        int maxSubArray = nums[0];
         
-        int ls = nums[0], rs = nums[0];
-        
-        for(int i=1; i < nums.length; i++)
+        for(int i = 1; i < nums.length; i++)
         {
-            //rs = Math.max(rs + nums[i], nums[i]);
-            if(rs + nums[i] > nums[i])
-            {
-                rs = rs + nums[i];
-            }
+            if(nums[i] > nums[i] + currentSubArray)
+                currentSubArray = nums[i];
             else
-            {
-                rs = nums[i];
-            }
-            
-            if(ls < rs)
-            {
-                ls = rs;
-            }            
+                currentSubArray = nums[i] + currentSubArray;
+            if(maxSubArray < currentSubArray)
+                maxSubArray = currentSubArray;
         }
-        return ls;
+        
+        return maxSubArray;
+        
+        //*****Given solution*****
+        /*
+        // Initialize our variables using the first element.
+        int currentSubarray = nums[0];
+        int maxSubarray = nums[0];
+        
+        // Start with the 2nd element since we already used the first one.
+        for (int i = 1; i < nums.length; i++) {
+            int num = nums[i];
+            // If current_subarray is negative, throw it away. Otherwise, keep adding to it.
+            System.out.println("comparing between " + num + ", and " + (currentSubarray + num));
+            currentSubarray = Math.max(num, currentSubarray + num);            
+            System.out.println("new currentSubarray: " + currentSubarray);
+            maxSubarray = Math.max(maxSubarray, currentSubarray);
+            System.out.println("new maxSubarray: " + maxSubarray);
+        }
+        
+        return maxSubarray;
+        */
     }
 }
-    
