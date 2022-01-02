b6741b181ab49f3607ab915ecb3a65cc8d81a85d
Saurabh Sharma <saurabhsvits@gmail.com>
2022-01-02 18:37:58 -0500

    Update SpiralMatrix.java

--- SpiralMatrix.java
+++ SpiralMatrix.java
@@ -47,69 +47,34 @@ class Solution {
         return result;
     }
     
-    public List<Integer> myImpl(int[][] matrix)
-    {
-        if(matrix == null || matrix.length == 0) return null;
-        
-        int rowOffset = 0, colOffset = 0;
-        List<Integer> result = new ArrayList<Integer>();
-        boolean isTraversingRow = true;        
-        int size = matrix.length * matrix[0].length;
-        
-        while(rowOffset < matrix.length/2 && colOffset < matrix[0].length/2)
-        {
-            //if(isTraversingRow)
-            //{            
-            int row = rowOffset, col = colOffset;
-            //System.out.println("row " + row);
-            //System.out.println("col " + col);
-            //System.out.println("offset " + offset);
-            
-            while(col <= matrix[0].length - 1 - colOffset)
-            {
-                System.out.println("traversing row");
-                result.add(matrix[row][col]);
-                col++;
-            }
-            if(col > matrix[0].length - 1 - colOffset)
-                col--; 
-            row++;
-            while(row < matrix.length - rowOffset)
-            {
-                System.out.println("traversing col");
-                result.add(matrix[row][col]);
-                row++;
-            }
-            
-            if(row == matrix.length - rowOffset)
-                row--;
-            
-            if(rowOffset < matrix.length/2 && colOffset < matrix[0].length/2)
-            {
-                col = matrix[0].length - 2 - colOffset;
-                while(col > colOffset - 1)
-                {
-                    System.out.println("traversing row backwards");
-                    result.add(matrix[row][col]);
-                    col--;
-                }
-                if(col == colOffset - 1)
-                    col++;
-
-                row = matrix.length - 2 - rowOffset;
-                while(row > rowOffset)
-                {
-                    System.out.println("traversing col backwards");
-                    result.add(matrix[row][col]);
-                    row--;
-                }
-                if(row == rowOffset)
-                    row++;
-            }     
-            colOffset++;
-            rowOffset++;
-            //}
-        }
+    /*
+    // DFS. Faster than 100%.    
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> result = new ArrayList<>();
+        boolean[][] visited = new boolean[matrix.length][matrix[0].length];
+        dfs(matrix, 0, 0, false, result, visited);
         return result;
     }
+    
+    public void dfs(int[][] matrix, int row, int col, boolean goingUp, List<Integer> result, boolean[][] visited)
+    {
+        if(row < 0 || col < 0 || row >= matrix.length || col >= matrix[0].length)
+        {
+            return;
+        }
+        if(visited[row][col])
+            return;
+        
+        result.add(matrix[row][col]);
+        visited[row][col] = true;        
+        if(goingUp)
+            dfs(matrix, row - 1, col, true, result, visited);
+        
+        dfs(matrix, row, col + 1, false, result, visited);
+        dfs(matrix, row + 1, col, false, result, visited);
+        dfs(matrix, row, col - 1, false, result, visited);
+        dfs(matrix, row - 1, col, true, result, visited);        
+    }    
+}
+*/
 }
