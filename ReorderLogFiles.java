7bdf3b05529bce845fc694c3015852dfc9773961
Saurabh Sharma <saurabhsvits@gmail.com>
2022-03-11 18:15:43 -0500

    Update ReorderLogFiles.java

--- ReorderLogFiles.java
+++ ReorderLogFiles.java
@@ -1,34 +1,41 @@
 // Leetcode problem #937
 class Solution {
     public String[] reorderLogFiles(String[] logs) {
-     Comparator comp = new Comparator<String>(){
-         @Override
-         public int compare(String s1, String s2)
+        Arrays.sort(logs, new Comparator<String>()
          {
-            String[] spl1 = s1.split(" ",2);
-            String[] spl2 = s2.split(" ",2);            
-            boolean isDigit1 = Character.isDigit(spl1[1].charAt(0));
-            boolean isDigit2 = Character.isDigit(spl2[1].charAt(0)); 
-            if(!isDigit1 && !isDigit2)
-            {
-                int x = spl1[1].compareTo(spl2[1]);
-                if(x == 0)
-                    return (spl1[0]).compareTo((spl2[0]));
-                return x;
-            }                
-            else if(!isDigit1)
-            {
-                return -1;
-            }
-            else if(!isDigit2)
-            {
-                return 1;
-            }            
-            return 0;                            
-         }
-     };     
-     
-     Arrays.sort(logs, comp);
-     return logs;   
-   }
+             @Override
+             public int compare(String s1, String s2)
+             {
+                 int spaceInd = s1.indexOf(' ');
+                 String sub1 =  s1.substring(spaceInd + 1);                 
+                 spaceInd = s2.indexOf(' ');
+                 String sub2 = s2.substring(spaceInd + 1);                 
+                 if(sub1.charAt(0) >= '0' && sub1.charAt(0) <= '9')
+                 {
+                     if(sub2.charAt(0) >= '0' && sub2.charAt(0) <= '9')
+                     {                        
+                        return 0;   
+                     }                         
+                     else
+                     {                        
+                         return 1;
+                     }                         
+                 }
+                 else
+                 {
+                     if(sub2.charAt(0) >= '0' && sub2.charAt(0) <= '9')
+                     {                        
+                        return -1;
+                     }                         
+                     else 
+                     {
+                        if(sub1.compareTo(sub2) == 0)
+                            return s1.compareTo(s2);
+                         return sub1.compareTo(sub2);
+                     }
+                 }                 
+             }
+         });
+         return logs;
+    }
 }
