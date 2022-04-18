0ebf763cd437385690d8c7bd8291fcf070be8412
Saurabh Sharma <saurabhsvits@gmail.com>
2022-04-17 22:35:10 -0400

    Update TrapRainWater.java
    Update start value of i to 0 while calculating tallest

--- TrapRainWater.java
+++ TrapRainWater.java
@@ -1,14 +1,13 @@
 class Solution {
     
-    // Easiest to understand and most efficient.
-    // Only downside is a little extra code.
+    // Easiest to understand and most efficient
+    // only downside is a little extra code.
     // For slightly more efficient code and the best solution, refer:
-    // https://leetcode.com/problems/trapping-rain-water/discuss/17357/Sharing-my-simple-c++-code:-O(n)-time-O(1)-space
-    
+    //https://leetcode.com/problems/trapping-rain-water/discuss/17357/Sharing-my-simple-c++-code:-O(n)-time-O(1)-space
     public int trap(int[] height) {
         if(height.length < 3) return 0;
         int tallest = 0;
-        for(int i=1; i < height.length; i++)
+        for(int i=0; i < height.length; i++)
         {
             if(height[i] > height[tallest])
                 tallest = i;
