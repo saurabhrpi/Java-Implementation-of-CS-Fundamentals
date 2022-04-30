86646b0785298a3773b6bf6d34d736bbe92b319a
Saurabh Sharma <saurabhsvits@gmail.com>
2022-04-30 11:41:55 -0400

    Update CreateBSTFromSortedArray.java
    Added println statements for better understanding.

--- CreateBSTFromSortedArray.java
+++ CreateBSTFromSortedArray.java
@@ -18,17 +18,33 @@ class Solution {
         return createBST(nums, 0, nums.length - 1);
     }
     
+    // At every level we only care about creating the node for middle of the range passed
     public TreeNode createBST(int[] nums, int left, int right)
     {
+        // when we dont have a child node
         if(left > right)
+        {
+            System.out.println("returning null for left = " + left + " and right = " + right);
             return null;
+        }
+        
+        // when we reach the leaf node             
         if(left == right)
+        {
+            System.out.println("returning nums[left] = " + nums[left] + " for left/right = " + left);
             return new TreeNode(nums[left]);
+        }
+            
         
         int mid = (left + right)/2;
+        System.out.println("mid = " + mid +" for left = " + left + " and right = " + right);
         TreeNode root = new TreeNode(nums[mid]);
+        System.out.println("created root node at " + nums[mid]);
         root.left = createBST(nums,left,mid - 1);
+        System.out.println("returning back from left = " + (root.left == null?null :root.left.val));
         root.right = createBST(nums,mid + 1, right);
+        System.out.println("returning back from right = " + (root.right == null?null :root.right.val));
+        System.out.println("returning root = " + root.val);
         return root;
     }
 }
