0895e9340c8bdbd105ba9b03e55e50aeeb508866
Saurabh Sharma <saurabhsvits@gmail.com>
2022-05-01 01:02:14 -0400

    Update PascalsTriangle.java
    Added a new clean approach

--- PascalsTriangle.java
+++ PascalsTriangle.java
@@ -1,29 +1,28 @@
 class Solution {
     public List<List<Integer>> generate(int numRows) {        
-        List<List<Integer>> rows = new ArrayList<List<Integer>>();        
-        return bruteForce(numRows, rows);
-        //return optimized(numRows, rows);
+        //List<List<Integer>> rows = new ArrayList<List<Integer>>();        
+        //return bruteForce(numRows, rows);
+        return cleanAndConcise(numRows);
     }
     
-    // for recursive solution, refer to :
-    // https://leetcode.com/problems/pascals-triangle/discuss/38220/Recursive-solution-Java.-1-ms
-    
-    //optimized() didn't work like 
-    //https://leetcode.com/problems/pascals-triangle/discuss/38182/Short-C%2B%2B-solution-using-only-O(k)-extra-space . Reason being once the list is updated at a lower level, the previous rows also get updated. 
-    public List<List<Integer>> optimized(int numRows, List<List<Integer>> rows)
+    public List<List<Integer>> cleanAndConcise(int numRows)
     {
-        List<Integer> r = new ArrayList<Integer>();
-        for(int i=0; i < numRows; i++)
+        List<List<Integer>> allrows = new ArrayList<List<Integer>>();
+        ArrayList<Integer> row = new ArrayList<Integer>();
+        for(int i=0;i<numRows;i++)
         {
-            r.add(1);
-            for(int j=i-1; j>0; j--)
-            {
-                r.set(j,(r.get(j) + r.get(j-1)));
-            }            
-            rows.add(r);
-        }        
-        return rows;
+            row.add(0, 1); // Add 1 at every level in the first position. 
+            // Once we reach level = 3, start updating from the cell right next to the
+            // newly added cell. Value will be = existing value of the cell + value of its next cell.
+            // Exactly like shown in the gif in the question.
+            for(int j=1;j<row.size()-1;j++) // won't touch the last element.
+                row.set(j, row.get(j)+row.get(j+1));
+            allrows.add(new ArrayList<Integer>(row));
+        }
+        return allrows;
+
     }
+            
     
     //space complexity can be reduced to O(n) from O(n^2).
     public List<List<Integer>> bruteForce(int numRows, List<List<Integer>> rows)
