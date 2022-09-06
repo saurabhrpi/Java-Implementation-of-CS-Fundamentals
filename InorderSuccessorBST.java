363d310bb1d79901ae29524e75f344e2f7df13ab
Saurabh Sharma <saurabhsvits@gmail.com>
2022-09-05 20:37:57 -0400

    Replacing with more intuitive approach
    Added method to print tree

--- InorderSuccessorBST.java
+++ InorderSuccessorBST.java
@@ -9,50 +9,62 @@ class Node {
 */
 
 class Solution {
-    public Node inorderSuccessor(Node node) {        
-        return helper(node);
+    public Node inorderSuccessor(Node node) {
+       //printTree(node);
+       if(node == null)
+           return null;
+        if(node.right != null)
+            return traverse(node.right);
+        if(node.parent == null)
+            return null;                
+        return findCorrectParent(node.parent, node);
+    }    
+    
+    public Node findCorrectParent(Node parent, Node node)
+    {
+        while(parent.parent != null)
+        {
+            if(parent.val > node.val)
+                return parent;
+            parent = parent.parent;
+        }
+        return parent.val > node.val? parent : null;
     }
     
-    public Node helper(Node node)
+    public void printTree(Node node)
+    {
+        while(node.parent != null)
+            node = node.parent;
+        Queue<Node> queue = new LinkedList<>();
+        int size = 0;
+        queue.offer(node);        
+        while(!queue.isEmpty())
+        {
+            size = queue.size();
+            for(int i = 0; i < size; i++)
+            {
+                Node n = queue.poll();                
+                System.out.println("processing root =" + n.val);
+                if(n.left != null)
+                {
+                    System.out.println("added left = " + n.left.val);
+                    queue.offer(n.left);
+                }
+                if(n.right != null)
+                {
+                    System.out.println("added right = " + n.right.val);
+                    queue.offer(n.right);
+                }
+            }
+        }
+    }
+    
+    public Node traverse(Node node)
     {
         if(node == null)
-            return null;        
-        if(node.parent == null)
-        {
-            return findLeftmostChildOfRight(node);
-        }
-        
-        // done for the case when node is on lhs of its parent.
-        if(node == node.parent.left)
-        {
-            Node lmcr = findLeftmostChildOfRight(node);
-            if(lmcr != null)
-                return lmcr;
-            return node.parent;            
-        }  
-        
-        // case when node is on the RHS of its parent
-        if(node == node.parent.right)
-        {      
-            Node lmcr = findLeftmostChildOfRight(node);
-            if(lmcr != null)
-                return lmcr;
-            Node p = node.parent;
-            while(p != null && p.parent != null && p == p.parent.right)
-                p = p.parent;
-            return (p == null? null : p.parent);
-        }
-        return null;
-    }
-    
-    public Node findLeftmostChildOfRight(Node node)
-    {
-        Node r = node.right, curr = null;
-        while(r != null)
-        {
-            curr = r;
-            r = r.left;
-        }
-        return curr;
+            return null;
+        if(node.left != null)
+            return traverse(node.left);
+        return node;
     }
 }
